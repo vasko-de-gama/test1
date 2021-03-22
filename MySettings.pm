bc5f340bffd72a5d34da9c1fe1dcade4bfb91d8b
vasko-de-gama <v.nikitenko@list.ru>
2021-03-22 23:15:05 +0700

    Update MySettings.pm

--- MySettings.pm
+++ MySettings.pm
@@ -1,8 +1,8 @@
 package MySettings;
 
 use constant {
-	DB_NAME => 'MyDB',
-	DB_HOST => '127.0.0.1',
+  DB_NAME => 'MyDB',
+  DB_HOST => '127.0.0.1',
   DB_PORT => 3311,
   DB_LOGIN => 'root',
   DB_PASSWORD => '',
